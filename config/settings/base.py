68d080eb60a80dc60d9706fa51b787a5c46b1780
mark-cunningham <cunniman13@gmail.com>
2019-02-17 19:22:12 +0000

    Go

--- config/settings/base.py
+++ config/settings/base.py
@@ -38,9 +38,20 @@ USE_TZ = True
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': env.db('DATABASE_URL', default='postgres://localhost/test'),
+}"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Impact',
+        'USER': 'postgres',
+        'PASSWORD': '#cun-POS-13!*!',
+        'HOST': '',
+        'PORT': '5432',
+    }
 }
+
 DATABASES['default']['ATOMIC_REQUESTS'] = True
 
 # URLS
